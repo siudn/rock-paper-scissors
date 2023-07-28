a239e8b5ff7bfcf6f2d63c02a59dda6028dfdd07
siudn <cshadx@gmail.com>
2023-07-28 16:11:51 -0400

    Created score function and displayed score

--- game.js
+++ game.js
@@ -1,30 +1,60 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const body = document.querySelector("body");
+
+const divButtons = document.createElement("div");
+divButtons.setAttribute("id","buttons");
 
 const rockButton = document.createElement("button");
 rockButton.classList.add("button");
 rockButton.setAttribute("id","rock");
 rockButton.textContent = "Rock";
+rockButton.addEventListener("click", () => {
+    score(playRound("Rock", getComputerChoice()));
+    displayedPlayerScore.textContent = `Your Score: ${playerScore}`;
+    displayedComputerScore.textContent = `CPU Score: ${computerScore}`;
+})
 
 const paperButton = document.createElement("button");
 paperButton.classList.add("button");
 paperButton.setAttribute("id","paper");
 paperButton.textContent = "Paper";
+paperButton.addEventListener("click", () => {
+    score(playRound("Paper", getComputerChoice()));
+    displayedPlayerScore.textContent = `Your Score: ${playerScore}`;
+    displayedComputerScore.textContent = `CPU Score: ${computerScore}`;
+})
 
 const scissorsButton = document.createElement("button");
 scissorsButton.classList.add("button");
 scissorsButton.setAttribute("id","scissors");
 scissorsButton.textContent = "Scissors";
+scissorsButton.addEventListener("click", () => {
+    score(playRound("Scissors", getComputerChoice()));
+    displayedPlayerScore.textContent = `Your Score: ${playerScore}`;
+    displayedComputerScore.textContent = `CPU Score: ${computerScore}`;
+})
 
-const divButtons = document.createElement("div");
-divButtons.setAttribute("id","buttons");
+const divResults = document.createElement("div")
+divResults.setAttribute("id","results");
+
+const displayedPlayerScore = document.createElement("p");
+displayedPlayerScore.classList.add("score");
+displayedPlayerScore.textContent = `Your Score: `;
+
+const displayedComputerScore = document.createElement("p");
+displayedComputerScore.classList.add("score");
+displayedComputerScore.textContent = `CPU Score: `;
 
 body.appendChild(divButtons);
 divButtons.appendChild(rockButton);
 divButtons.appendChild(paperButton);
 divButtons.appendChild(scissorsButton);
 
-let playerChoice;
-let computerChoice;
+body.appendChild(divResults);
+divResults.appendChild(displayedPlayerScore);
+divResults.appendChild(displayedComputerScore);
 
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
@@ -48,26 +78,29 @@ function playRound(playerSelection, computerSelection) {
     else if (pCompare === "PAPER" && cCompare === "SCISSORS") {
         return "You Lose! Scissors beats Paper";
     }
-    else if (pCompare === "SCISSORS" && cCompare === "SCISSORS") {
-        return "Tie!";
-    }
     else if (pCompare === "ROCK" && cCompare === "PAPER") {
         return "You Lose! Paper beats Rock";
-    }
-    else if (pCompare === "PAPER" && cCompare === "PAPER") {
-        return "Tie!";
     }
     else if (pCompare === "SCISSORS" && cCompare === "PAPER") {
         return "You Win! Scissors beats Paper";
     }
-    else if (pCompare === "ROCK" && cCompare === "ROCK") {
-        return "Tie!";
-    }
     else if (pCompare === "PAPER" && cCompare === "ROCK") {
         return "You Win! Paper beats Rock";
     }
-    else {
+    else if (pCompare === "SCISSORS" && cCompare === "ROCK") {
         return "You Lose! Rock beats Scissors";
+    }
+    else {
+        return "Tie!";
+    }
+}
+
+function score(result) {
+    if (result.includes("Win")) {
+        playerScore++;
+    }
+    else if (result.includes("Lose")) {
+        computerScore++;
     }
 }
 
